4bed07e3c1cfe14cc4ebd0bbff72cea8c4d4f0dd
FazaHaya <162892114+FazaHaya@users.noreply.github.com>
2025-04-09 16:32:11 +0700

    Update KisahController.php

--- app/Http/Controllers/KisahController.php
+++ app/Http/Controllers/KisahController.php
@@ -7,7 +7,22 @@ use Illuminate\Http\Request;
 class KisahController extends Controller
 {
     public function show($slug)
-    {
-        return view('kisah.detail', compact('slug'));
+{
+    $tokoh = [
+        ['nama' => 'Pierre Tendean', 'slug' => 'pierre-tendean', 'gambar' => 'pierre1.jpg'],
+        ['nama' => 'Ahmad Yani', 'slug' => 'ahmad-yani', 'gambar' => 'ahmad_yani.jpg'],
+        ['nama' => 'Di Panjaitan', 'slug' => 'di-panjaitan', 'gambar' => 'di-panjaitan.jpg'],
+        ['nama' => 'Mt Haryono', 'slug' => 'mt-haryono', 'gambar' => 'mt-haryono.jpg'],
+        ['nama' => 'Jenderal S Parman', 'slug' => 's-parman', 'gambar' => 's-parman.jpg'],
+    ];
+
+    $item = collect($tokoh)->firstWhere('slug', $slug);
+
+    if (!$item) {
+        abort(404);
     }
+
+    return view('kisah.detail', compact('slug', 'item'));
+}
+
 }
